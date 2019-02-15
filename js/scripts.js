63ed3c1215aabfd5e9c81d13dfcb593603663fe5
Stewart ilondanga <stilondanga@gmail.com>
2019-02-15 20:06:24 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,26 +11,28 @@ $('document').ready(() => {
       var controller = new ScrollMagic.Controller();
       // arrow down
       new ScrollMagic.Scene({
-        triggerElement: "#part-1",
-        triggerHook: "onLeave",
-        duration: "50",
-      })
-      /*			.setTween("#arrow-down", 1, {opacity: 0 })
-      			//.addIndicators()
-      			.addTo(controller);
-      	// parts
-      	$('.part').each(function(ind, elem){
-      		const thisId = "#"+$(elem).attr('id');
-      		const nextId = "#"+$(elem).attr('data-next');
-      		console.log(nextId)
-      		new ScrollMagic.Scene({
-      						triggerElement: thisId,
-      						triggerHook: "onEnter",
-      						duration: "0",
-      						reverse: false,
-      		})
-      			.setTween(thisId, 1, {transform: "translateX(0)" })
-      			//.addIndicators()
-      			.addTo(controller);
-      	});
-      });
+          triggerElement: "#part-1",
+          triggerHook: "onLeave",
+          duration: "50",
+        })
+        .setTween("#arrow-down", 1, {
+          opacity: 0
+        })
+        //.addIndicators()
+        .addTo(controller);
+      // parts
+      $('.part').each(function(ind, elem) {
+            const thisId = "#" + $(elem).attr('id');
+            const nextId = "#" + $(elem).attr('data-next');
+            console.log(nextId)
+            new ScrollMagic.Scene({
+              triggerElement: thisId,
+              triggerHook: "onEnter",
+              duration: "0",
+              reverse: false,
+            })
+            /*      			.setTween(thisId, 1, {transform: "translateX(0)" })
+                  			//.addIndicators()
+                  			.addTo(controller);
+                  	});
+                  });
